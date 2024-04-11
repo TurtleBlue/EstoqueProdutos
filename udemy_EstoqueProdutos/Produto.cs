af07f10bcc392c79f7a2270e24035ff74a74a01e
Vinicius Hideaki Menda Nakano <34291484+TurtleBlue@users.noreply.github.com>
2024-04-11 18:14:53 -0300

    Add files via upload

---
+++ udemy_EstoqueProdutos/Produto.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy_EstoqueProdutos
+{
+    internal class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto (string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set 
+            { 
+                if (_nome != null && _nome.Length > 1)
+                {
+                    _nome = Nome;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get { return _preco; }   
+            set { _preco = Preco; }
+        }
+        
+        public int Quantidade
+        {
+            get { return _quantidade; }
+            set { _quantidade = Quantidade; }
+        }
+
+        /*
+        public Produto(string Nome, double Preco, int Quantidade)
+        {
+            nome = Nome;
+            preco = Preco;
+            quantidade  = Quantidade;
+        }
+
+        public Produto (string Nome, double Preco)
+        {
+            nome = Nome;
+            preco = Preco;
+            quantidade = 5;
+        }
+        */
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int Quantidade)
+        {
+            _quantidade = _quantidade + Quantidade;
+        }
+        public void RemoverProdutos(int Quantidade)
+        {
+            _quantidade = _quantidade - Quantidade;
+        }
+    }
+}
---
+++ udemy_EstoqueProdutos/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace udemy_EstoqueProdutos
+{
+    /* Fazer um programa para ler os dados de um estoque (nome, preço e quantidade no estoque). 
+     * Em seguida:
+     * - Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no estoque)
+     * - Realizar uma entrada no estoque e mostrar novamente os dados do produto
+     * - Realizar uma saída no estoque e mostrar novamente os dados do produto
+     * 
+     * Para resolver esse problema, deve-se criar uma classe.
+     * */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao, quantidade, loop = 1;
+            string nome;
+            double preco;
+
+            Console.WriteLine("DIGITE OS DADOS DO PRODUTO\n");
+
+            Console.WriteLine("Nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Preço: ");
+            preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade: ");
+            quantidade = int.Parse(Console.ReadLine());
+
+            Produto produto = new Produto(nome, preco, quantidade);
+
+            do
+             {
+                Console.WriteLine($"Dados do produto: {produto.Nome}, {produto.Preco}, {produto.Quantidade}, Total:R${produto.Quantidade * produto.Preco:n2}");
+                Console.WriteLine("Deseja adicionar ou remover produtos ao estoque?");
+                Console.WriteLine("1 - Adicionar");
+                Console.WriteLine("2 - Remover");
+                Console.WriteLine("4 - Sair");
+                opcao = int.Parse(Console.ReadLine());
+                 switch (opcao)
+                 {
+                     case 1:
+                         Console.WriteLine("Quantos produtos deseja adicionar?");
+                         produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+                         Console.Clear();
+                         Console.WriteLine($"Novos dados do produto: " +
+                             $"Produto: {produto.Nome}, " +
+                             $"Preco: {produto.Preco}, " +
+                             $"Quantidade: {produto.Quantidade}, " +
+                             $"Total:R${produto.ValorTotalEmEstoque():n2}");
+                         break;
+                     case 2:
+                         Console.WriteLine("Quantos produtos deseja remover?");
+                         produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+                         Console.WriteLine($"Novos dados do produto: " +
+                             $"Produto: {produto.Nome}, " +
+                             $"Preco: {produto.Preco}, " +
+                             $"Quantidade: {produto.Quantidade}, " +
+                             $"Total:R${produto.Quantidade * produto.Preco:n2}");
+                         break;
+                        case 4:
+                         Console.Write($"Certo, saindo do programa. \n" +
+                              $"O relatório final ficou em: " +
+                              $"Produto: {produto.Nome}, " +
+                              $"Preco: {produto.Preco}, " +
+                              $"Quantidade: {produto.Quantidade} " +
+                              $"Total:R${produto.Quantidade * produto.Preco:n2}\n");
+                         loop = 0;
+                         break;
+                     default:
+                         Console.WriteLine("Opção inválida, tente novamente");
+                         Console.ReadKey();
+                         Console.Clear();
+                         break;
+                 }
+             } while (loop == 1); 
+        }
+    }
+}
